666a60d436482a3fd6128e4d046ccc9a8afb4c64
josimar-jr <shikamarujunior@gmail.com>
2018-01-19 23:33:27 -0200

    adicionando as instrucoes para comando correto de roteamento entre as paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { TodoComponent } from './todo/todo.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'todo', component: TodoComponent },
 
   // redirect to home
   { path: '**', redirectTo: '' }
