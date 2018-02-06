a07650916e1d4b2dd8e304f1db9dbb656b7ddf97
josimar-jr <shikamarujunior@gmail.com>
2018-02-06 13:44:04 -0200

    inclusao das classes Todo, User e TodoItem / atualizacao da home para renderizar os todos de um usuario

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User, Todo } from '../models/index';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  currentUser : User;
+  todos : Todo[] = [];
 
   constructor( private router: Router ) { }
 
   ngOnInit() {
+    // capturar o usuário logado ou redirecionar para login
+
+    // monta a lista de todos conforme o usuário
+    for(let x = 1; x < 10; x++){
+      let todoTemp = new Todo();
+      todoTemp.id =  x;
+      todoTemp.title = `Todo ${x}`;
+      todoTemp.created_at = Date.now();
+      todoTemp.updated_at = Date.now();
+
+      this.todos.push(todoTemp);
+    }
+
   }
 
 }
